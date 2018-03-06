0b41e913ef7b8f2889a015cc792824db32d6cf57
Aaron <aaronmassey45@gmail.com>
2018-03-06 15:52:03 -0600

    Editted initial state and redefined success case

--- src/reducers/movie-reducer.js
+++ src/reducers/movie-reducer.js
@@ -1,11 +1,25 @@
 import { REQUEST, SUCCESS, FAILURE } from '../actions';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  results: [],
+  selectedMovie: {
+    title: '',
+    tagline: '',
+    overview: '',
+    poster: '',
+    backdrop: '',
+    rating: '',
+    runtime: '',
+    release: '',
+    revenue: '',
+  }
+};
 
 export default function movieData(state = INITIAL_STATE, action) {
   switch (action.type) {
     case SUCCESS:
-      return { ...state, data: action.payload };
+    console.log(action.payload);
+      return { ...state, results: action.payload.results };
     default:
       return state;
   }

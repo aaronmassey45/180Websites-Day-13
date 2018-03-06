20a9c72d2b1a04d594b13a0f895f4e342c47a22d
Aaron <aaronmassey45@gmail.com>
2018-03-06 17:05:27 -0600

    Updated initial state and added case for select_success

--- src/reducers/movie-reducer.js
+++ src/reducers/movie-reducer.js
@@ -1,26 +1,42 @@
-import { REQUEST, SUCCESS, FAILURE } from '../actions';
+import { REQUEST, SELECT_SUCCESS, SELECT_FAILURE, SEARCH_SUCCESS, SEARCH_FAILURE } from '../actions';
 
 const INITIAL_STATE = {
-  results: [],
-  selectedMovie: {
-    title: '',
-    tagline: '',
-    overview: '',
-    poster: '',
-    backdrop: '',
-    rating: '',
-    runtime: '',
-    release: '',
-    revenue: '',
-  }
+	results: [],
+	selectedMovie: {
+		title: '',
+		tagline: '',
+		overview: '',
+		poster_path: '',
+		backdrop_path: '',
+		vote_average: '',
+		runtime: '',
+		release_date: '',
+		revenue: '',
+	},
 };
 
 export default function movieData(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    case SUCCESS:
-    console.log(action.payload);
-      return { ...state, results: action.payload.results };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case SEARCH_SUCCESS:
+			return { ...state, results: action.payload.results };
+		case SELECT_SUCCESS:
+			const { title, tagline, overview, poster_path, backdrop_path, vote_average, runtime, release_date, revenue } = action.payload;
+			return {
+				...state,
+				results: [],
+				selectedMovie: {
+					title,
+					tagline,
+					overview,
+					poster_path,
+					backdrop_path,
+					vote_average,
+					runtime,
+					release_date,
+					revenue,
+				},
+			};
+		default:
+			return state;
+	}
 }

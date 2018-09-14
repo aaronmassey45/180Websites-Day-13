453e644f578ed899953ce63b8a3ab9b94e70c540
Aaron <aaronmassey45@gmail.com>
2018-09-14 15:39:04 -0500

    Created movie component

---
+++ src/components/Movie.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Movie = ({ movie }) => {
+  return (
+    <div className="col-md-6">
+      <div className="card mb-4 shadow-sm">
+        <div className="card-body">
+          <h5>{movie.title}</h5>
+          <p className="card-text">
+            <b>
+              <i className="fas fa-play" /> Released
+            </b>
+            : {movie.release_date}
+            <br />
+            <b>
+              <i className="fas fa-compact-disc" /> Rating
+            </b>
+            : {movie.voter_average}
+          </p>
+          <Link to={`/movies/${movie.id}`} className="btn btn-dark btn-block">
+            <i className="fas fa-chevron-right" /> View Info
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Movie;

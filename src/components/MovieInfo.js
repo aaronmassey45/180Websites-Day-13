1110371ece71b7673002d46cb9c6f7c5c7b784b4
Aaron <aaronmassey45@gmail.com>
2018-09-17 07:44:24 -0500

    Created component to view details about movie

---
+++ src/components/MovieInfo.js
@@ -0,0 +1,101 @@
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
+
+import Spinner from './spinner/Spinner';
+
+export default class MovieInfo extends Component {
+  state = {
+    movie: {},
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${
+          this.props.match.params.id
+        }?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+      )
+      .then(res => {
+        this.setState({ movie: res.data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  componentWillUnmount() {
+    document.body.style.backgroundImage = null;
+  }
+
+  arrayToString = array => {
+    return array.map(item => item.name).join(', ');
+  };
+
+  renderMovieDetails = movie => {};
+
+  render() {
+    const { movie } = this.state;
+    if (movie === undefined || Object.keys(movie).length === 0) {
+      return <Spinner />;
+    }
+
+    const IMG = 'https://image.tmdb.org/t/p/w500/';
+    document.body.style.backgroundImage = `url(${IMG}${movie.backdrop_path})`;
+
+    return (
+      <div className="card movie-details bg-dark text-white">
+        <div className="card-header">
+          <Link to="/" className="btn btn-secondary btn-sm">
+            Go Back
+          </Link>
+        </div>
+        <div className="card-body row ">
+          <div className="col-md-4">
+            <img
+              src={`${IMG}${movie.poster_path}`}
+              alt="Movie poster"
+              className="img-fluid"
+            />
+          </div>
+          <div className="card-text col-md backdrop">
+            <h1>{movie.title.toUpperCase()}</h1>
+            <h4 className="text-gold">{movie.tagline}</h4>
+            <div className="mb-4">{movie.overview}</div>
+
+            <h5 className="text-gold">{this.arrayToString(movie.genres)}</h5>
+            <h6 className="mb-4">
+              {this.arrayToString(movie.production_companies)}
+            </h6>
+            <div className="row">
+              <div className="col-6 mb-2">
+                <h5 className="mb-0">Original Release:</h5>{' '}
+                <h4>
+                  <Moment format="YYYY-MM-DD" className="text-gold">
+                    {movie.release_date}
+                  </Moment>
+                </h4>
+              </div>
+              <div className="col-6 mb-2">
+                <h5 className="mb-0">Runtime:</h5>{' '}
+                <h4 className="text-gold">{movie.runtime} min</h4>
+              </div>
+              <div className="col-6 mb-2">
+                <h5 className="mb-0">Box Office:</h5>
+                <h4 className="text-gold">
+                  $
+                  {movie.revenue
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                </h4>
+              </div>
+              <div className="col-6 mb-2">
+                <h5 className="mb-0">Vote Average</h5>{' '}
+                <h4 className="text-gold">{movie.vote_average} / 10</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}

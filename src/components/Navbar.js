5d8dd3dee6b619da9cad8d551b1c0a60c7a09d4a
Aaron <aaronmassey45@gmail.com>
2018-09-18 10:15:01 -0500

    Removed links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,7 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-dark bg-dark mb-5">
-      <span className="navbar-brand mb-0 h1 mx-auto">TMDb Search!</span>
-      <div className="navbar-nav text-center">
-        <a className="nav-item nav-link" href="https://aaronmassey.pro/">
-          My Portfolio
-        </a>
-        <a
-          className="nav-item nav-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://medium.com/@aaronmassey45"
-        >
-          Blog
-        </a>
-        <a
-          className="nav-item nav-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/aaronmassey45"
-        >
-          GitHub
-        </a>
-      </div>
+      <span className="navbar-brand mb-0 h1 mx-auto">tMDB Search!</span>
     </nav>
   );
 };
